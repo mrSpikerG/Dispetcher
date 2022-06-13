87cb2634a41294a77a07affeabc480c7502f7422
Fili_vw23@student.itstep.org <Fili_vw23@student.itstep.org>
2022-06-13 22:43:22 +0300

    Добавьте файлы проекта.

---
+++ Dispetcher/MainViewModel.cs
@@ -0,0 +1,80 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispetcher
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        private ProcessAdvanced selectedModel;
+        public ProcessAdvanced SelectedModel {
+            get => this.selectedModel;
+            set
+            {
+                this.selectedModel = value;
+                OnPropertyChanged("SelectedModel");
+            }
+        }
+        public ObservableCollection<ProcessAdvanced> ProcessCollection { get; set; }
+
+        public MainViewModel()
+        {
+            //SelectedModel.ProcessName
+            this.ProcessCollection = new ObservableCollection<ProcessAdvanced>();
+            this.ProcessCollection = this.getProcesses();
+        //    this.ProcessCollection.Add(new ProcessAdvanced("test"));
+            //this.ProcessCollection = this.getProcesses();
+        }
+
+        //получение процессов
+        private ObservableCollection<ProcessAdvanced> getProcesses()
+        {
+            ObservableCollection<ProcessAdvanced> TEMP = new ObservableCollection<ProcessAdvanced>();
+
+            foreach (var item in Process.GetProcesses())
+            {
+                try {
+                    TEMP.Add(new ProcessAdvanced(item));
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return TEMP;
+        }
+
+        private RelayCommand removeCommand;
+
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ?? (removeCommand = new RelayCommand(() =>
+                {
+                        SelectedModel.Process.Kill();
+                        ProcessCollection.Remove(SelectedModel);
+                }));
+            }
+            set { }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+    }
+}
---
+++ Dispetcher/Model/ProcessAdvanced.cs
@@ -0,0 +1,94 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Dispetcher
+{
+    public class ProcessAdvanced : ViewModelBase, INotifyPropertyChanged
+    {
+        public ProcessAdvanced(Process process)
+        {
+            this.Process = process;
+            PerformanceCounter cpuCounter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName);
+            double cpu = cpuCounter.NextValue();
+            cpu = cpuCounter.NextValue();
+            this.CPU = $"{Math.Round(cpu)}%";
+
+            PerformanceCounter ramCounter = new PerformanceCounter("Process", "Working Set", process.ProcessName);
+            double ram = ramCounter.NextValue() / (1000 * 1000);
+            this.RAM = $"{Math.Round(ram, 1)} Mb";
+
+            this.getCPU();
+            this.getRAM();
+        }
+
+
+        private Process process;
+        public Process Process
+        {
+            get => process;
+            set => SetProperty<Process>(ref process, value);
+        }
+
+        private string cpu;
+        private string ram;
+        public string CPU
+        {
+            get => cpu;
+            set => SetProperty<string>(ref cpu, value);
+        }
+        public string RAM
+        {
+            get => ram;
+            set => SetProperty<string>(ref ram, value);
+        }
+
+        public void getCPU()
+        {
+            DispatcherTimer ProcTimerCPU = new DispatcherTimer();
+            ProcTimerCPU.Tick += CPUgetCustomEvent;
+            ProcTimerCPU.Interval = new TimeSpan(0, 0, 1);
+            ProcTimerCPU.Start();
+        }
+
+        private void CPUgetCustomEvent(object sender, EventArgs e)
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName);
+            double cpu = cpuCounter.NextValue();
+            cpu = cpuCounter.NextValue();
+            this.CPU = $"{Math.Round(cpu)}Мб/c";
+        }
+
+        public void getRAM()
+        {
+            DispatcherTimer ProcTimerRAM = new DispatcherTimer();
+            ProcTimerRAM.Tick += RAMgetCustomEvent;
+            ProcTimerRAM.Interval = new TimeSpan(0, 0, 1);
+            ProcTimerRAM.Start();
+        }
+
+        private void RAMgetCustomEvent(object sender, EventArgs e)
+        {
+            PerformanceCounter ramCounter = new PerformanceCounter("Process", "Working Set", process.ProcessName);
+            double ram = ramCounter.NextValue() / (1000 * 1000);
+            this.RAM = $"{Math.Round(ram, 1)} Mb";
+        }
+
+       
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
